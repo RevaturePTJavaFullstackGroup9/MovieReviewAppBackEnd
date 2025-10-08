2fb2ea50b25385b1a6727c58b478394c7bd5cce7
Carlos Aguilar <77519735+ssbucarlos@users.noreply.github.com>
2025-10-08 17:13:22 -0500

    initial commit

---
+++ src/main/java/com/revature/movie_review_back_end/MovieReviewBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.revature.movie_review_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewBackEndApplication.class, args);
+	}
+
+}
